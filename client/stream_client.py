520e7e44b33bc9ae212a1ff3e41c5d5e06baa13c
Ricky-Hao <a471558277@gmail.com>
2017-07-31 14:28:28 +0800

    Add while to client

--- client/stream_client.py
+++ client/stream_client.py
@@ -6,18 +6,18 @@ client_socket = socket.socket()
 client_socket.connect(('angela.moe', 45837))
 
 connection = client_socket.makefile("wb")
+while True:
+    try:
+        camera = picamera.PiCamera()
+        camera.resolution = (640,480)
+        camera.framerate = 24
+        camera.start_preview()
+        time.sleep(2)
 
-try:
-    camera = picamera.PiCamera()
-    camera.resolution = (640,480)
-    camera.framerate = 24
-    camera.start_preview()
-    time.sleep(2)
+        camera.start_recording(connection, format='h264')
+        camera.wait_recording(30)
+        camera.stop_recording()
 
-    camera.start_recording(connection, format='h264')
-    camera.wait_recording(30)
-    camera.stop_recording()
-
-finally:
-    connection.close()
-    client_socket.close()
+    finally:
+        connection.close()
+        client_socket.close()
