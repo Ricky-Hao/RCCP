62452feaf5d857059a490ebe48e10374f644c720
Ricky-Hao <a471558277@gmail.com>
2017-07-31 19:14:52 +0800

    Fix bug

--- client/stream_client.py
+++ client/stream_client.py
@@ -16,13 +16,14 @@ class PiCamera:
 
     def LoggerInit(self):
         os.environ['TZ'] = "Asia/Shanghai"
-        self.logger = logging.getLogger(type(self).__name)
+        self.logger = logging.getLogger(type(self).__name__)
         self.logger.setLevel(logging.INFO)
         ch = logging.StreamHandler()
         ch.setLevel(logging.INFO)
-        formatter = logging.Formatter("'%(asctime)s - %(name)s - %(levelname)s - %(message)s'")
+        formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         ch.setFormatter(formatter)
         self.logger.addHandler(ch)
+        self.logger.info("Logger init.")
 
     def CameraInit(self):
         try:
@@ -32,12 +33,12 @@ class PiCamera:
             self.logger.critical("Please close all the program which use the picamera.")
         self.logger.info("PiCamera started.")
 
-        self.camera.resolution = self.config['resolution']
-        self.logger.info("PiCamera resolution: "+self.config['resolution'])
+        self.camera.resolution = tuple(self.config['resolution'])
+        self.logger.info("PiCamera resolution: "+str(self.config['resolution']))
         self.camera.framerate = self.config['framerate']
-        self.logger.info("PiCamera framerate: "+self.config['framerate'])
+        self.logger.info("PiCamera framerate: "+str(self.config['framerate']))
         self.camera.rotation = self.config['rotation']
-        self.logger.info("PiCamera rotation: "+self.confg['rotation'])
+        self.logger.info("PiCamera rotation: "+str(self.config['rotation']))
 
         self.camera.start_preview()
         time.sleep(2)
@@ -48,7 +49,7 @@ class PiCamera:
             self.client_socket = socket.socket()
             self.client_socket.connect_ex((self.config['server_ip'],self.config['server_port']))
             self.connection = self.client_socket.makefile("wb")
-            self.logger.info("Connected to "+self.config['server_ip']+":"+self.config['server_port']+'.')
+            self.logger.info("Connected to "+self.config['server_ip']+":"+str(self.config['server_port'])+'.')
         except BaseException as err:
             self.logger.error(err)
             self.ClientStart()
@@ -59,7 +60,7 @@ class PiCamera:
         self.logger.info("Connection closed.")
 
     def ReadConfig(self):
-        self.script_path = os.path.realpath(__path__)
+        self.script_path = os.path.realpath(__file__)
         self.dir_path = os.path.dirname(self.script_path)
         try:
             with open(self.dir_path+"/client.json", "r") as f:
@@ -74,3 +75,6 @@ class PiCamera:
         self.camera.wait_recording(self.config['wait_recording'])
         self.stop_recording()
 
+
+if __name__ == "__main__":
+    PiCamera()
