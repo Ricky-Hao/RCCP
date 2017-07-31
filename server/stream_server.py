0b5f31042a0d40117964fe430883b102a6e6c32f
Ricky-Hao <a471558277@gmail.com>
2017-07-31 17:02:02 +0800

    Fix StreamSaver logger

--- server/stream_server.py
+++ server/stream_server.py
@@ -56,6 +56,11 @@ class StreamSaver:
     def LoggerInit(self):
         self.logger = logging.getLogger(type(self).__name__)
         self.logger.setLevel(logging.INFO)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
 
     def run(self):
         self.h264_filename = "Camera_"+self.timestamp+".h264"
