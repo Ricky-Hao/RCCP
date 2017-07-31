4151e11de0d919b1e5fdd690c1f70b4d5fcaa53f
Ricky-Hao <a471558277@gmail.com>
2017-07-31 15:00:21 +0800

    Catch error PiCameraMMALError.

--- client/stream_client.py
+++ client/stream_client.py
@@ -2,22 +2,27 @@ import socket
 import time
 import picamera
 
-client_socket = socket.socket()
-client_socket.connect(('angela.moe', 45837))
+def Connect():
+    global client_socket
+    client_socket = socket.socket()
+    client_socket.connect(('angela.moe', 45837))
+    return client_socket.makefile("wb")
 
-connection = client_socket.makefile("wb")
+
 while True:
     try:
         camera = picamera.PiCamera()
         camera.resolution = (1280,720)
         camera.framerate = 24
+        camera.rotation = 270
         camera.start_preview()
         time.sleep(2)
-
-        camera.start_recording(connection, format='h264')
+        connection=Connect()
+        camera.start_recording(connection, format='h264', quality=25)
         camera.wait_recording(300)
         camera.stop_recording()
-
-    finally:
         connection.close()
         client_socket.close()
+
+    except PiCameraMMALError:
+        time.sleep(10)
