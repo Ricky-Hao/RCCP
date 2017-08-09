769d20019cd321494060fad05b80385d48396dd0
Ricky-Hao <a471558277@gmail.com>
2017-08-09 18:21:11 +0800

    Update web

--- web/web/web.py
+++ web/web/web.py
@@ -1,62 +1,39 @@
 import os
 import sqlite3
+import json
+from functools import wraps
+from base64 import b64encode, b64decode
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-        render_template, flash
+        render_template, flash, send_from_directory
 
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config.from_json('config.json')
+if app.config.get('SECRET_KEY') is None:
+    app.logger.info("Generate SECRET_KEY")
+    with open('config.json', 'r') as f:
+        j = json.load(f)
 
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'web.db'),
-    SECRET_KEY='5702039',
-    USERNAME='Ricky',
-    PASSWORD='5702039'
-    ))
-app.config.from_envvar('FLASK_SETTINGS', silent=True)
+    j['SECRET_KEY'] = b64encode(os.urandom(64)).decode()
+    
+    with open('config.json','w') as f:
+        json.dump(j, f)
+    app.config.from_json('config.json')
+app.config['SECRET_KEY'] = b64decode(app.config.get('SECRET_KEY'))
+app.logger.debug(app.config)
 
-def connect_db():
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not session.get('logged_in'):
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
-def get_db():
-    if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-@app.teardown_appcontext
-def close_db(error):
-    if hasattr(g, 'sqlite_db'):
-        g.sqlite_db.close()
-
-def init_db():
-    db = get_db()
-    with app.open_resource('schema.sql', mode='r') as f:
-        db.cursor().executescript(f.read())
-    db.commit()
-
-@app.cli.command('initdb')
-def initdb_command():
-    init_db()
-    print('Initialized the database.')
 
 @app.route('/')
-def show_entries():
-    db = get_db()
-    cur = db.execute('select id, title, text from entries order by id desc')
-    entries = cur.fetchall()
-    return render_template('show_entries.html', entries=entries)
-
-@app.route('/add', methods=['POST'])
-def add_entry():
-    if not session.get('logged_in'):
-        abort(401)
-    db = get_db()
-    db.execute('insert into entries (title, text) values (?, ?)',
-            [request.form['title'], request.form['text']])
-    db.commit()
-    flash('New entry was successfully posted.')
-    return redirect(url_for('show_entries'))
+@login_required
+def index():
+    return 'It works!'
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -71,19 +48,31 @@ def login():
         else:
             session['logged_in'] = True
             flash('You were logged in.')
-            return redirect(url_for('show_entries'))
+            return redirect(url_for('show_videos'))
     return render_template('login.html', error=error)
 
 @app.route('/logout')
+@login_required
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out.')
-    return redirect(url_for('show_entries'))
+    return redirect(url_for('show_videos'))
 
-@app.route('/article/<int:post_id>')
-def article(post_id):
-    db = get_db()
-    cur = db.execute('select title, text from entries where id = %d' % post_id)
-    article = cur.fetchone()
-    return render_template('article.html', article=article)
-    
+@app.route('/video')
+@login_required
+def show_videos():
+    video_walk = os.walk(app.config.get('VIDEO_PATH'))
+    video_list = []
+    for dp, dn, fn in video_walk:
+        for f in fn:
+            video_list.append(f)
+    return render_template('video.html', video_list=video_list)
+
+@app.route('/video/<string:video_name>')
+@login_required
+def video(video_name):
+    return send_from_directory(app.config.get('VIDEO_PATH'), video_name)
+
+
+if __name__ == "__main__":
+    app.run()
